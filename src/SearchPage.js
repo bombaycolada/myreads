8440a324302106d2f817936285a8767bb21f4440
Ola <olataylor@hotmail.co.uk>
2017-08-31 20:25:52 +0100

    Create Link on Search Page that links back to Main Page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class SearchPage extends Component {
 
 	render(){
-		return <div>I am a Search Page</div>
+		return (
+			<div>
+				I am a Search Page
+				<p><Link to="/">Back to Main Page</Link></p>
+			</div>
+
+		)
 	}
 }
 
